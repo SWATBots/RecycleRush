6abef796d35e0c6c45ce7c629f2e9876fda48631
Will Allen <nightforager@gmail.com>
2015-01-24 10:50:40 -0700

    Initial commit

---
+++ Robot1/src/com/SWATBots/FRC2015/robot/DriveControl.java
@@ -0,0 +1,25 @@
+package com.SWATBots.FRC2015.robot;
+
+public class DriveControl {
+
+	private double Power = 0.0; 
+	
+	public double calculateSpeed(double Stick){
+    	return Stick*Power;
+    }
+	
+	public void choosePower (boolean setFast)
+	{
+	if(setFast)
+	{
+		Power = 1.0;
+		
+	}
+	else{
+		
+		Power = 0.5;
+	}
+	
+}
+
+}
