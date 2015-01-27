25f39bb09ba5fe09e46634912732d91f61d95ea6
Robotiks98 <Robotiks98@gmail.com>
2015-01-26 20:29:39 -0700

    Drive control is updated with data from Samuel.

--- Robot1/src/com/SWATBots/FRC2015/robot/DriveControl.java
+++ Robot1/src/com/SWATBots/FRC2015/robot/DriveControl.java
@@ -4,35 +4,36 @@ import edu.wpi.first.wpilibj.*;
 
 //This class is for drive speed control calculations with speed limit buttons
 
-
+ 
 public class DriveControl {
 
-	public RobotDrive DriveTrain; // robot drive system
-	double Kp = 0.03;
+       public RobotDrive DriveTrain; // robot drive system
+       double Kp = 0.03;
 
-	private double Power = 0.0; 
-	
-	
-DriveControl (Gyro gyro)	
-{	
+       private double Power = 0.0;
+
+      
+DriveControl (Gyro gyro)  
+{     
+
 }
-	public double calculateSpeed(double Stick){
-    	return Stick*Power;
+
+       public double calculateSpeed(double Stick){
+       return Stick*Power;
     }
-	
-	public void choosePower (boolean setFast)
-	{
-	if(setFast)
-	{
-		Power = 1.0;
-		
-	}
-	else{
-		
-		Power = 0.5;
-	}
-	  
-	
+
+
+       public void choosePower (boolean setFast)
+       {
+
+       if(setFast)
+       {
+             Power = 1.0;   
+       }
+       else{
+             Power = 0.5;
+       }
+
   }
 
 }
